4d0b0d7c6ae8e830c2f7fba3d2cb3899e151ad69
Bekir - CDI <b.burakcebecii@gmail.com>
2022-08-08 15:40:21 +0200

    added context..

---
+++ src/AppContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useReducer } from 'react';
+
+export const AppContext = createContext();
+
+const initialState = {
+	count: 0,
+};
+
+function reducer(state, action) {
+	const _state = { ...state };
+	switch (action) {
+		case 'increaseCount':
+			_state.count++;
+			break;
+		case 'decreaseCount':
+			_state.count--;
+			break;
+	}
+	return _state;
+}
+
+export const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<AppContext.Provider
+			value={{
+				state,
+				dispatch,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};

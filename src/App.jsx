48da43eea0f724cd30165b54208976b344e1b4ca
Bekir - CDI <b.burakcebecii@gmail.com>
2022-08-08 15:38:50 +0200

    decrease and increase number button added..

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
-import "./App.scss";
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
+	const { state, dispatch } = useContext(AppContext);
 
-  return (
-    <div className="App">
-      <h1>Hallo</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Site with useContext/useReducer</h1>
+			<p>test: {state.count}</p>
+			<div className="buttonArea">
+				<button onClick={() => dispatch('decreaseCount')}>-</button>
+				<button onClick={() => dispatch('increaseCount')}>+</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
